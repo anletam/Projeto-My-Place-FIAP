3d989d7109eeaa8a8486bfde1f28a6582c2299d3
Andreia Leticia <98561592+anletam@users.noreply.github.com>
2022-03-21 19:09:39 -0300

    Add files via upload

---
+++ Projeto My Place/assets/js/scroll.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', () => {
+
+    const navBar = document.querySelector('.nav-bar');
+    // console.log(window.scrollY);
+    navBar.classList.toggle('active-scroll', window.scrollY > 100)
+});
